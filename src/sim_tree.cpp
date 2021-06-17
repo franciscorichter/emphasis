818e3e43086ce5095f9f5705b09592db82c32f03
Thijs Janzen <thijsjanzen@gmail.com>
2021-06-17 10:58:12 +0200

    Update sim_tree.cpp

--- src/sim_tree.cpp
+++ src/sim_tree.cpp
@@ -6,15 +6,32 @@ using namespace Rcpp;
 
 namespace emphasis {
 
-  tree_t sample_ext_spec(const tree_t& in_tree) {
+  int sample_ext_spec(const tree_t& in_tree, int clade) {
     
+    auto is_extinct[](const node_t& nt) {(nt.to == 1 && 
+                                          nt.t_ext == && 
+                                          nt.clade == c) ? return true : return false;}
     
-    
-    ext_spec = sample(which(tree$to == 1 & 
-      tree$t_ext == Inf & 
-      tree$clade == clade), 1)
+    auto n = std::count_if(in_tree.begin(),
+                           in_tree.end(),
+                           is_extinct );
+
+    int selected = std::uniform_int_distribution<> (0, static_cast<int>(n - 1))(reng);
+
+    int index = 0;
+    for (auto i : in_tree) {
+      if (is_extinct(i)) {
+        selected--;
+        if (selected == 0) {
+          return index;
+        }
+      }
+      index++;
+    }
+    return 0; // * this should NEVER be reached* 
+    // perhaps add a throw?
   }
-  
+
   
 // [[Rcpp::export(name = "simtree_pd_cpp")]]
 tree_t simtree_pd(const std::vector<double>& pars,
@@ -27,7 +44,6 @@ tree_t simtree_pd(const std::vector<double>& pars,
   double P = 0.0;
   
   tree_t tree = emphasis::detail::create_tree(std::vector<double>(2, 0.0), 2);
-  
   
   while (cbt < ct &&
          N1 >= 1  &&
@@ -65,15 +81,38 @@ tree_t simtree_pd(const std::vector<double>& pars,
             
             auto ext_spec = sample_ext_spec(tree);
             
-            ext_spec = sample(which(tree$to == 1 & 
-              tree$t_ext == Inf & 
-              tree$clade == clade), 1)
-            
-            tree$t_ext[ext_spec] = next_event_time
-            auto removed_branch_length = tree$t_ext[ext_spec] - tree$brts[ext_spec]
+            tree[ext_spec].t_ext = next_event_time;
+            auto removed_branch_length = tree[ext_spec].t_ext - tree[ext_spec].brts;
             P -= removed_branch_length
           }
         }
-      
+        /* 
+         * I don't really understand how to translate this to C++:
+         * tree = rbind(tree,
+                       data.frame(brts = next_event_time,
+                       to = to,
+                       t_ext = Inf,
+                       clade = clade))
+         
+         // insert_species does not have a "to" ?
+         // also, why insert ALWAYS, and not only if speciation occured?
+         insert_species(next_event_time, to, )
+         */
+      }
+    }
+    cbt = next_event_time
   }
+ 
+ if ((N1 == 0) |  (N2 == 0)) {
+   tree = cbt
+ } else {
+   // again:
+   /*
+   tree = rbind(tree,data.frame(brts = ct,
+                                to = 1,
+                                t_ext = Inf, 
+                                clade = 0))
+    */
+ }
+ 
   
