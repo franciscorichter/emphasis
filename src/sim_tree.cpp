eaf1d58df3e3fb622754652a5d06d54d42b0b4e8
Thijs Janzen <thijsjanzen@gmail.com>
2021-06-17 11:08:50 +0200

    add sim_tree_dd

--- src/sim_tree.cpp
+++ src/sim_tree.cpp
@@ -57,15 +57,16 @@ tree_t simtree_pd(const std::vector<double>& pars,
 
     double rate_max = (lambda_max + mu) * N 
     double u1 = std::uniform_real_distribution<>()(reng);
-    double next_event_time = cbt - std::log(u1) / lambda_max;
+    double next_event_time = cbt - std::log(u1) / rate_max;
     P += N * (next_event_time - cbt)  ;
     if (next_event_time < next_bt) {
-      double u2 = std::uniform_real_distribution<>()(reng);
+      
       double lambda_ct = std::max(0, 
                           pars[1] + pars[2] * N  +  
                           ((P + N * (next_event_time - cbt) - next_event_time) / N) * pars[3]);
       double pt = ((lambda_ct + mu) * N ) / rate_max;
       
+      double u2 = std::uniform_real_distribution<>()(reng);
       if (u2 < pt) {
         
         int to = std::bernoulli_distribution(lambda_ct / (lambda_ct + mu))(reng); // c(1, 0) with prob c(lambda_ct, mu) / (lambda_ct + mu)
@@ -114,5 +115,71 @@ tree_t simtree_pd(const std::vector<double>& pars,
                                 clade = 0))
     */
  }
+ return tree;
+}
+
+// [[Rcpp::export(name = "simtree_dd_cpp")]]
+tree_t simtree_dd(const std::vector<double>& pars,
+                  double ct) {
  
+ double cbt = 0.0;
+  int N1 = 1;
+  int N2 = 1;
+  double mu = std::max(pars[0], 0.0); 
   
+  tree_t tree = emphasis::detail::create_tree(std::vector<double>(2, 0.0), 2);
+  
+  while (cbt < ct &&
+         N1 >= 1  &&
+         N2 >= 1) {
+    double next_bt = emphasis::get_next_bt(tree, cbt);
+    
+    int N = N1 + N2;
+    double lambda_ct = std::max(0.0, 
+                                pars[1] + pars[2] * N);
+    double rate_max = (lambda_ct + mu) * N;
+    double u1 = std::uniform_real_distribution<>()(reng);
+    double next_event_time = cbt - std::log(u1) / rate_max;
+    if (next_event_time < ct) {
+      int to = std::bernoulli_distribution(lambda_ct / (lambda_ct + mu))(reng); // c(1, 0) with prob c(lambda_ct, mu) / (lambda_ct + mu)
+      int clade = std::bernoulli_distribution(N2 / (N1 + N2))(reng); // c("a","b"), e.g. c(0, 1) with prob (N1, N2) / (N1 + N2)
+      
+      if (to == 1) {
+        clade == 0 ? N1++ : N2++;
+      } else {
+        clade == 0 ? N1-- : N2--;
+        
+        /*
+         if ((N1 >= 1) &  (N2 >= 1)) {
+           ext_spec = sample(which(tree$to == 1 & 
+                            tree$t_ext == Inf & 
+                           tree$clade == clade), 
+                           1)
+           tree$t_ext[ext_spec] = next_event_time
+         }
+         */
+      }
+      /*
+       tree = rbind(tree,
+       data.frame(brts = next_event_time,
+                     to = to,
+                     t_ext = Inf,
+                     lambda = lambda_ct, 
+                     clade = clade))
+       */
+    }
+    cbt = next_event_time
+  }
+  if ((N1 == 0) |  (N2 == 0)) {
+    tree = cbt
+  } else {
+    tree = rbind(tree,data.frame(brts = ct,
+                                 to = 1,
+                                 t_ext = Inf,
+                                 lambda = lambda_ct, 
+                                 clade = 0))
+  }
+  return(tree)
+}
+      
+    
