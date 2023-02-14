28688591275c838806e752640b49310eed674452
Thijs Janzen <thijsjanzen@gmail.com>
2023-02-14 09:57:59 +0100

    add testing function

--- src/rcpp_mce.cpp
+++ src/rcpp_mce.cpp
@@ -195,3 +195,47 @@ Rcpp::NumericMatrix rcpp_mce_grid(const Rcpp::NumericMatrix pars_R,
 
 
 
+
+//' @export
+ // [[Rcpp::export(name = "e_cpp_test")]]
+ List rcpp_mce_test(const std::vector<double>& brts,       
+                    const std::vector<double>& init_pars,      
+                    int sample_size,
+                    int maxN,
+                    int soc,
+                    int max_missing,               
+                    double max_lambda,             
+                    const std::vector<double>& lower_bound,  
+                    const std::vector<double>& upper_bound,  
+                    double xtol_rel,                     
+                    int num_threads)
+ {
+   auto model = emphasis::Model(lower_bound, upper_bound);
+   
+   auto E = emphasis::E_step(sample_size,
+                             maxN,
+                             init_pars,
+                             brts,
+                             model,
+                             soc,
+                             max_missing,
+                             max_lambda,
+                             num_threads);
+   List ret;
+   List trees;
+   for (const emphasis::tree_t& tree : E.trees) {
+     trees.push_back(unpack(tree));
+   }
+   ret["trees"] = trees;
+   ret["rejected"] = E.rejected;
+   ret["rejected_overruns"] = E.rejected_overruns;
+   ret["rejected_lambda"] = E.rejected_lambda;
+   ret["rejected_zero_weights"] = E.rejected_zero_weights;
+   ret["time"] = E.elapsed;
+   ret["weights"] = E.weights;
+   ret["fhat"] = E.fhat;
+   return ret;
+ }
+
+
+
