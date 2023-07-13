1492d2a33bcbbe667f8b2f8856e7fbf7e1c8aa77
franciscorichter <franciscorichter@gmail.com>
2023-07-13 11:03:49 +0200

    Updating generatePhyloDDD

--- R/DD_simulation.R
+++ R/DD_simulation.R
@@ -1,11 +1,11 @@
-generatePhyloDDD <- function(n_trees, lambda0_c,mu_c,carrying_capacity_c,age1,ddmodel1, ss_check = TRUE){
+generatePhyloDDD <- function(n_trees, mu_c,lambda0_c,beta_c,age1, ss_check = TRUE){
   
   trees <- list()
   extrees <- list()
   Lmats <- list()
   brds_s <- list()
   
-  name.param <- c("lambda0", "mu","K","age") 
+  name.param <- c("mu", "lambda","beta","age") 
   true.param <- vector(mode='list', length=4)
   names(true.param) <- name.param
   
@@ -13,13 +13,18 @@ generatePhyloDDD <- function(n_trees, lambda0_c,mu_c,carrying_capacity_c,age1,dd
     
     lambda0_sample <- runif(1, min = lambda0_c[1], max = lambda0_c[2])
     mu_sample <- runif(1, min = mu[1], max = mu[2])
-    carrying_capacity_sample <- rdunif(1, min = carrying_capacity_c[1], max = carrying_capacity_c[2])
-    crown_time_sample <- rdunif(1, min = age1[1], max = age1[2])
+    beta_sample <- runif(1, min = beta_c[1], max = beta_c[2])
+    crown_time_sample <- runif(1, min = age1[1], max = age1[2])
     
-    vec.param <- c(lambda0_sample,mu_sample,as.integer(carrying_capacity_sample),as.integer(crown_time_sample))
-    sim.param <- c(lambda0_sample,mu_sample,as.integer(carrying_capacity_sample))
+    vec.param <- c(lambda0_sample,mu_sample,beta_sample,crown_time_sample)
+    sim.param <- c(lambda0_sample,mu_sample,beta_sample,0)
     
-    outputs <-  DDD::dd_sim(pars = sim.param, age = crown_time_sample, ddmodel = ddmodel1)
+    #outputs <-  DDD::dd_sim(pars = sim.param, age = crown_time_sample, ddmodel = ddmodel1)
+    outputs <- sim_single_tree_pd_cpp(pars = sim.param,
+                                           max_t = crown_time_sample,
+                                           max_lin = 1e+10,
+                                           max_tries = 100)
+    
     
     tree  <- outputs[[1]]
     extree  <- outputs[[2]]
@@ -56,22 +61,22 @@ generatePhyloDDD <- function(n_trees, lambda0_c,mu_c,carrying_capacity_c,age1,dd
   return(out)
 }
 
-fname_ddd<- function(n_trees, lambda0,mu,carrying_capacity,age,dd_mod){
+fname_ddd<- function(n_trees, lambda0,mu,beta,age,dd_mod){
   
   lambd_text <- ifelse(length(lambda0) == 2, paste(lambda0[1], lambda0[2], sep="-"), 
                        as.character(lambda0))
   mu_text <- ifelse(length(mu) == 2, paste(mu[1], mu[2], sep="-"), 
                     as.character(mu))
   
-  k_text <- ifelse(length(carrying_capacity) == 2, paste(carrying_capacity[1], carrying_capacity[2], sep="-"), 
-                   as.character(carrying_capacity))
+  k_text <- ifelse(length(beta) == 2, paste(beta[1], beta[2], sep="-"), 
+                   as.character(beta))
   
   age_text <- as.character(age)
   
   dd_mod_text <- as.character(dd_mod)
   
   
-  fname <- paste("nt", n_trees,"la0",lambd_text,"mu",mu_text,"k",k_text,"age",age_text,"ddmod",dd_mod_text ,sep="-")
+  fname <- paste("nt", n_trees,"la0",lambd_text,"mu",mu_text,"beta",k_text,"age",age_text,"ddmod",dd_mod_text ,sep="-")
   
   return(fname)
   
