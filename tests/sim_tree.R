18660852d794271f98882ff1521e2a2125e4cc50
franciscorichter <franciscorichter@gmail.com>
2023-07-19 10:46:40 +0200

    update general simulation function for NN

--- tests/sim_tree.R
+++ tests/sim_tree.R
@@ -1,13 +1,31 @@
 library(emphasis)
-pars = c(0.1,0.8,-0.0175,0)
+#pars = c(0.1,0.8,-0.0175,0)
+pars = c(0.24624818,  0.81232484, -0.01140782,  0.00000000)
 max_t = 20
 
-pars = pars*max_t
-max_lin = 1000000
+max_lin = 100000
 max_tries = 1
 
 #sim_single_tree_pd_cpp
-tree=simulate_single_pd_tree_cpp(pars = pars,
+tree=sim.tree(pars = pars*max_t,
                        max_t = 1,
                        max_lin = max_lin,
                        max_tries = max_tries)
+
+tree
+
+sim.tree <- function(pars,
+                     max_t = 1,
+                     max_lin,
+                     max_tries){
+  it <- try(sim_tree_pd_cpp(pars = pars*max_t,
+                            max_t = 1,
+                            max_lin = max_lin,
+                            max_tries = max_tries),silent = TRUE)
+  if(class(it)!="try-error"){
+    result = it
+  }else{
+    result = NaN
+  }
+  return(result)
+}
